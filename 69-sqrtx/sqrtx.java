86f6ade9d02c58a4cacadc2db38f8b2ca9cf172b
Vivek Kumar <185205704+vivekkushwahaofficial@users.noreply.github.com>
2026-07-06 00:17:00 +0530

    Time: 1 ms (97.66%) | Memory: 42.9 MB (8.25%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,36 +1,26 @@
 class Solution {
     public int mySqrt(int x) {
-
-        // Handle the special case
         if (x == 0) {
             return 0;
         }
-
         int low = 1;
         int high = x;
+        int mid = 0;
         int answer = 0;
 
         while (low <= high) {
-
-            // Find the middle element
-            int mid = low + (high - low) / 2;
-
-            // Use long to avoid integer overflow
-            long square = (long) mid * mid;
-
+            mid = low + (high - low) / 2;
+            long square = (long)mid * mid;
             if (square == x) {
-                return mid; // Perfect square found
-            } 
-            else if (square < x) {
-                answer = mid;      // Store possible answer
-                low = mid + 1;     // Search on the right
-            } 
-            else {
-                high = mid - 1;    // Search on the left
+                return mid;
+            } else if (square < x) {
+                answer = mid;
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
             }
         }
-
-        // Return the floor value of the square root
         return answer;
     }
 }
